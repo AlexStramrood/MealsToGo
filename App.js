e55dc0a63770496d125493b4c4f89acdb28bbbd1
Alex Stramrood <astramrood@gmail.com>
2022-04-12 15:04:17 +0200

    Console Log removed

--- App.js
+++ App.js
@@ -1,26 +1,29 @@
-import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, Platform, StatusBar} from 'react-native';
-
-/* const isAndroid = Platform.OS === 'android' */
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+} from "react-native";
 
 console.log(StatusBar.currentHeight);
 
 export default function App() {
   return (
     <>
-    <SafeAreaView style={{flex: 1, marginTop: StatusBar.currentHeight }}>
-      <View style={{padding: 16, backgroundColor:"green"}}>
-        <Text>This is the search bar</Text>
+      <SafeAreaView style={{ flex: 1, marginTop: StatusBar.currentHeight }}>
+        <View style={{ padding: 16, backgroundColor: "green" }}>
+          <Text>This is the search bar</Text>
         </View>
-      <View style={{flex: 1, padding: 16, backgroundColor:"blue"}}>
-        <Text>This will be the list</Text>
-       </View>
-        <ExpoStatusBar style='auto' />
-    </SafeAreaView>
+        <View style={{ flex: 1, padding: 16, backgroundColor: "blue" }}>
+          <Text>This will be the list</Text>
+        </View>
+        <ExpoStatusBar style="auto" />
+      </SafeAreaView>
     </>
   );
 }
 
-const styles = StyleSheet.create({
-
-});
+const styles = StyleSheet.create({});
